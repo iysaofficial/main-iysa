687ecde7cf07b3d67e92a5451d9518a990f0ba07
kamal <kamal.putraa255@gmail.com>
2023-07-31 20:15:24 +0700

    Revisi 31 Juli

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,8 +3,8 @@ import "../css/navigation.css";
 
 import React, { useState, useEffect } from "react";
 import { FaChevronDown, FaHouse } from "react-icons/fa6";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -101,85 +101,85 @@ const Navigation = () => {
 
   return (
     <header className="header">
-        <div className="header-main">
-          <div className="logo">
-            <img src="./assets/images/logo/navigation-logo.WebP" />
-          </div>
-          <div className="open-nav-menu">
-            <span></span>
-          </div>
-          <div className="menu-overlay"></div>
-          <nav className="nav-menu">
-            <div className="close-nav-menu">
-              {/* <img
+      <div className="header-main">
+        <div className="logo">
+          <img src="./assets/images/logo/navigation-logo.WebP" />
+        </div>
+        <div className="open-nav-menu">
+          <span></span>
+        </div>
+        <div className="menu-overlay"></div>
+        <nav className="nav-menu">
+          <div className="close-nav-menu">
+            {/* <img
                 src="./assets/images/logo/icons8-close.svg"
                 alt="close"
               /> */}
-              <FontAwesomeIcon icon={faXmark} />
-            </div>
-            <ul className="menu">
-              <li className="menu-item mt-0">
-                <a href="/">
-                  <FaHouse /> Home
-                </a>
-              </li>
-              <li className="menu-item">
-                <a href="/About">About</a>
-              </li>
-              <li className="menu-item menu-item-has-children">
-                <a href="#" data-toggle="sub-menu">
-                  Curation <FaChevronDown />
-                </a>
-                <ul className="sub-menu text-center">
-                  <li className="menu-item">
-                    <a href="/CurationOSPC">Curation OSPC</a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="/CurationLKTIN">Curation LKTIN</a>
-                  </li>
-                </ul>
-              </li>
-              <li className="menu-item menu-item-has-children">
-                <a href="#" data-toggle="sub-menu">
-                  Our Team
-                  <FaChevronDown />
-                </a>
-                <ul className="sub-menu text-center">
-                  <li className="menu-item">
-                    <a href="/ExpertTeam">Expert Team</a>
-                  </li>
-                  <li className="menu-item">
+            <FontAwesomeIcon icon={faXmark} />
+          </div>
+          <ul className="menu">
+            <li className="menu-item mt-0">
+              <a href="/">
+                <FaHouse /> Home
+              </a>
+            </li>
+            <li className="menu-item">
+              <a href="/About">About</a>
+            </li>
+            <li className="menu-item menu-item-has-children">
+              <a href="#" data-toggle="sub-menu">
+                Curation <FaChevronDown />
+              </a>
+              <ul className="sub-menu text-center">
+                <li className="menu-item">
+                  <a href="/CurationOSPC">Curation OSPC</a>
+                </li>
+                <li className="menu-item">
+                  <a href="/CurationLKTIN">Curation LKTIN</a>
+                </li>
+              </ul>
+            </li>
+            <li className="menu-item menu-item-has-children">
+              <a href="#" data-toggle="sub-menu">
+                Our Team
+                <FaChevronDown />
+              </a>
+              <ul className="sub-menu text-center">
+                <li className="menu-item">
+                  <a href="/ExpertTeam">Expert Team</a>
+                </li>
+                {/* <li className="menu-item">
                     <a href="/ExpertStaff">Expert Staff</a>
-                  </li>
-                </ul>
-              </li>
-              <li className="menu-item">
-                <a href="/NewsletterPage">Newsletter</a>
-              </li>
-              <li className="menu-item menu-item-has-children">
-                <a href="#" data-toggle="sub-menu">
-                  Gallery <FaChevronDown />
-                </a>
-                <ul className="sub-menu text-center">
-                  <li className="menu-item">
-                    <a href="/GalleryPhoto">Photo</a>
-                  </li>
-                  <li className="menu-item">
-                    <a href="/GalleryVideo">Video</a>
-                  </li>
-                </ul>
-              </li>
-              <li className="menu-item">
-                <a href="/FaqPage">FAQ</a>
-              </li>
-              <li className="menu-item">
-                <a href="/ContactPage">Contact Us</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+                  </li> */}
+              </ul>
+            </li>
+            <li className="menu-item">
+              <a href="/NewsletterPage">Newsletter</a>
+            </li>
+            <li className="menu-item menu-item-has-children">
+              <a href="#" data-toggle="sub-menu">
+                Gallery <FaChevronDown />
+              </a>
+              <ul className="sub-menu text-center">
+                <li className="menu-item">
+                  <a href="/GalleryPhoto">Photo</a>
+                </li>
+                <li className="menu-item">
+                  <a href="/GalleryVideo">Video</a>
+                </li>
+              </ul>
+            </li>
+            <li className="menu-item">
+              <a href="/FaqPage">FAQ</a>
+            </li>
+            <li className="menu-item">
+              <a href="/ContactPage">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
       {/* </div> */}
     </header>
   );
 };
-export default Navigation;
+export default Navigation;
--- src/components/newsletter/NewsletterComp.js
+++ src/components/newsletter/NewsletterComp.js
@@ -3,87 +3,83 @@ import React, { useState } from "react";
 // Import Data JSON
 import DataLetter from "../../data/newsletter/NewsletterData.json";
 
-
 const NewsletterComp = () => {
+  const [selectedFilter, setSelectedFilter] = useState("all"); // Menambahkan state untuk menyimpan filter yang dipilih
 
-    const [selectedFilter, setSelectedFilter] = useState("all"); // Menambahkan state untuk menyimpan filter yang dipilih
+  // Fungsi untuk mengubah filter yang dipilih
+  const handleFilterChange = (filter) => {
+    setSelectedFilter(filter);
+  };
 
-    // Fungsi untuk mengubah filter yang dipilih
-    const handleFilterChange = (filter) => {
-        setSelectedFilter(filter);
-    };
-
-
-    return (
-        <section className="newsletter-section" id="newsletter-section">
-            <div className="container">
-                <div className="newsletter-wrapper">
-                    <div className="controls">
-                        <button
-                            type="button"
-                            className="control"
-                            onClick={() => handleFilterChange("duapuluhdua")}
-                        >
-                            2023
-                        </button>
-                        <button
-                            type="button"
-                            className="control"
-                            onClick={() => handleFilterChange("duapuluhtiga")}
-                        >
-                            2022
-                        </button>
+  return (
+    <section className="newsletter-section" id="newsletter-section">
+      <div className="container">
+        <div className="newsletter-wrapper">
+          <div className="controls">
+            <button
+              type="button"
+              className="control"
+              onClick={() => handleFilterChange("duapuluhdua")}
+            >
+              2023
+            </button>
+            <button
+              type="button"
+              className="control"
+              onClick={() => handleFilterChange("duapuluhtiga")}
+            >
+              2022
+            </button>
+          </div>
+          {selectedFilter !== "duapuluhtiga" && (
+            <>
+              <div className="row">
+                {DataLetter.duapuluhtiga.map((Letter) => {
+                  return (
+                    <div className="col-lg-3 col-md-6 col-sm-12">
+                      <div className="box mx-auto" key={Letter.id}>
+                        <div className="mx-auto text-center">
+                          <img
+                            className="newsletter-image img-fluid mx-auto"
+                            src={Letter.gambar}
+                          ></img>
+                          <div className="read-more-overlay">
+                            <a href={Letter.linkbtn}>Read More</a>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    {selectedFilter !== "duapuluhtiga" && (
-                        <>
-                            <div className="row">
-                                {DataLetter.duapuluhtiga.map((Letter) => {
-                                    return (
-                                        <div className="box " key={Letter.id}>
-                                            <div className="heading text-center">
-                                                <h4>{Letter.title}</h4>
-                                            </div>
-                                            <div>
-                                                <img className="mt-4" src={Letter.gambar}></img>
-                                            </div>
-                                            <div className="text">
-                                                <p>{Letter.content}
-                                                </p>
-                                                <a href={Letter.linkbtn}>Read more</a>
-                                            </div>
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        </>
-                    )}
-                    {selectedFilter !== "duapuluhdua" && (
-                        <>
-                            <div className="row">
-                                {DataLetter.duapuluhdua.map((Letter) => {
-                                    return (
-                                        <div className="box"  key={Letter.id}>
-                                            <div className="heading text-center">
-                                                <h4>{Letter.title}</h4>
-
-                                            </div>
-                                            <div>
-                                                <img className="mt-4" src={Letter.gambar}></img>
-                                            </div>
-                                            <div className="text">
-                                                <p>{Letter.content}
-                                                </p>
-                                                <a href={Letter.linkbtn}>Read more</a>
-                                            </div>
-                                        </div>  
-                                    );
-                                })}
-                            </div>
-                        </>
-                    )}
-                </div>
-            </div>
-        </section>
-    )
-}
-export default NewsletterComp 
+                  );
+                })}
+              </div>
+            </>
+          )}
+          {selectedFilter !== "duapuluhdua" && (
+            <>
+              <div className="row">
+                {DataLetter.duapuluhdua.map((Letter) => {
+                  return (
+                    <div className="col-lg-3 col-md-6 col-sm-12">
+                      <div className="box mx-auto" key={Letter.id}>
+                        <div className="mx-auto text-center">
+                          <img
+                            className="newsletter-image img-fluid mx-auto"
+                            src={Letter.gambar}
+                          ></img>
+                        </div>
+                        <div className="newsletter-button text-center">
+                          <a href={Letter.linkbtn}>Read more</a>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+export default NewsletterComp;
