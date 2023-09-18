acd99fe9765b6d3c1307322d5994a1e256f97be2
Nay <Nay@gmail.com>
2023-09-18 15:10:06 +0700

    update info curasi

--- src/components/infocurasi/Curasi.js
+++ src/components/infocurasi/Curasi.js
@@ -9,18 +9,9 @@ const Curasi = () => {
               <p className="">
               This information will display what events have been curated and are being curated by iysa with the ministry of culture, research and technology. 
               </p>
-              {/* <p>
-                Since its inception, IYSA has been active in various activities so
-                that it can be affiliated with many countries such as: Malaysia,
-                Thailand, Singapore, China, India, Korea, Taiwan, Turkey, Tunisia,
-                Egypt, Abu Dhabi and so on.{" "}
-              </p> */}
-              {/* <a href="/About" className="btn btn-primary">
-                See More
-              </a> */}
             </div>
   
-            <div className="col-md-6  hero-image text-center">
+            <div className="col-md-6">
               <iframe src="https://drive.google.com/uc?id=1bZko3snHRbtuTBa_2qnKqUquymQBImuB " className="mt-5" width="90%" height="570"/>
             </div>
           </div>
