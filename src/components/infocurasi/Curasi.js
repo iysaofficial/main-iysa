1556cf90d51dde6c92cf5f55bd792265a3eaf68b
Nay <Nay@gmail.com>
2023-09-18 15:40:45 +0700

    update info curasi

--- src/components/infocurasi/Curasi.js
+++ src/components/infocurasi/Curasi.js
@@ -1,24 +1,37 @@
 const Curasi = () => {
-    return (
-      <section className="hero-section">
-        <div className="container">
-          <div className="row ">
-            <div className="col-md-6 hero-content mt-auto mb-auto">
-              <h1 className="">Indonesian Young</h1>
-              <h1 className="">Scientist Association</h1>
-              <p className="">
-              This information will display what events have been curated and are being curated by iysa with the ministry of culture, research and technology. 
-              </p>
+  return (
+    <section className="hero-iframe">
+      <div className="container">
+        <div className="">
+          <div className="kotak row text-center col-12 mx-auto">
+            <div className="pag col-lg-4 col-12">
+              <img
+                src="https://drive.google.com/uc?id=1XiksqceoHO59D6Cd5m-61tdjQfexamEc"
+                alt="Footer Logo"
+                className="mt-5 mx-auto"
+                height="250"
+              ></img>
+              <h2 className="mt-3">
+                This information will display what events have been curated and
+                are being curated by iysa with the ministry of culture, research
+                and technology.
+              </h2>
+              {/* <p className="mt-3 text-center ">This information will display what events have been curated and
+                are being curated by iysa with the ministry of culture, research
+                and technology.</p> */}
             </div>
-  
-            <div className="col-md-6">
-              <iframe src="https://drive.google.com/uc?id=1bZko3snHRbtuTBa_2qnKqUquymQBImuB " className="mt-5" width="90%" height="570"/>
+            <div className="if text-center col-lg-8 col-12 mt-5 mb-5">
+              <iframe
+                src="https://drive.google.com/uc?id=1bZko3snHRbtuTBa_2qnKqUquymQBImuB"
+                width="80%"
+                height="570"
+              />
             </div>
           </div>
         </div>
-      </section>
-    );
-  };
-  
-  export default Curasi;
-  
+      </div>
+    </section>
+  );
+};
+
+export default Curasi;
