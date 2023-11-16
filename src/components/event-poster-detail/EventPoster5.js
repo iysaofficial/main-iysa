033c97a530853e3cde18c8708bdc02e496124366
naufal zidane <youngscientist.iysa@gmail.com>
2023-11-16 14:44:51 +0700

    iymia 2024

--- src/components/event-poster-detail/EventPoster5.js
+++ src/components/event-poster-detail/EventPoster5.js
@@ -2,22 +2,22 @@
 import '../../css/event/eventPoster.css';
 
 // Import FontAwesome
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   faLocationDot,
   faPlus,
   faMinus,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faSquareFacebook,
   faSquareTwitter,
   faLinkedin,
   faSquarePinterest,
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons';
 
 // Import Data
-import EventPosterDetail from "../../data/event/EventPosterDetail.json";
+import EventPosterDetail from '../../data/event/EventPosterDetail.json';
 
 function EventPoster5() {
   const [showContent, setShowContent] = useState(false);
@@ -26,7 +26,6 @@ function EventPoster5() {
     setShowContent(!showContent);
   };
   return (
-    
     <section>
       <div id="main-event" className="container">
         <div className="row event-wrapper ">
@@ -40,20 +39,46 @@ function EventPoster5() {
                 <br />
                 <div className="main-colom row col-12 mt-5 mb-5">
                   <div className="colom-left col-md-8  mt-2">
-                    <h1 className="fw-bolder">{EventPosterDetail5.title}</h1>
+                    <h1 className="fw-bolder">
+                      {EventPosterDetail5.title}
+                    </h1>
                     <p>Opening Event in 2024</p>
                     <p>2nd IYMIA🥳</p>
-                    <p>International Young Moslem Inventor Award (IYMIA)</p>
+                    <p>
+                      International Young Moslem Inventor Award
+                      (IYMIA)
+                    </p>
                     <br></br>
-                    <p>IYSA will hold 2nd IYMIA in collaboration with the Department of Food Science and Technology, Institut Pertanian Bogor.</p>
-                    <p>This event will take place in January 2024, conducted online and offline at the Institut Pertanian Bogor 👏🏻👏🏻</p>
+                    <p>
+                      IYSA will hold 2nd IYMIA in collaboration with
+                      the Department of Food Science and Technology,
+                      Institut Pertanian Bogor.
+                    </p>
+                    <p>
+                      This event will take place in January 2024,
+                      conducted online and offline at the Institut
+                      Pertanian Bogor 👏🏻👏🏻
+                    </p>
                     <p>-------------------------------------</p>
                     <p>Save the date ‼️</p>
                     <br></br>
                     <ul>
-                        <li><strong>📍 Registration Deadline:</strong>  December 15, 2023</li>
-                        <li><strong>📍 Payment Deadline:</strong> December 22, 2023</li>
-                        <li><strong>📍 Event Date:</strong> January 11-14, 2024</li>
+                      <li>
+                        <strong>📍 Registration Deadline:</strong>{' '}
+                        December 15, 2023
+                      </li>
+                      <li>
+                        <strong>📍 Payment Deadline:</strong> December
+                        22, 2023
+                      </li>
+                      <li>
+                        <strong>📍 Event Date:</strong> January 11-14,
+                        2024
+                      </li>
+                      <li>
+                        <strong>Place:</strong> Institut Pertanian
+                        Bogor, IPB Campus Dramaga
+                      </li>
                     </ul>
                     <br></br>
                     <p>Get the guide book in registration link‼️</p>
@@ -68,7 +93,9 @@ function EventPoster5() {
                       Registration Form
                     </a>
                     <br />
-                    <p className="hastag">{EventPosterDetail5.Hastag}</p>
+                    <p className="hastag">
+                      {EventPosterDetail5.Hastag}
+                    </p>
                   </div>
                   <div className="colom-right col-md-4 text-center text-md-start ">
                     <a className="btn btn-primary shadow mt-2">
@@ -79,7 +106,9 @@ function EventPoster5() {
                     <h4 className="fw-bolder">Date and Time</h4>
                     <p>{EventPosterDetail5.DateAdTime}</p>
                     <br />
-                    <h4 className="fw-bolder">Registration End Date</h4>
+                    <h4 className="fw-bolder">
+                      Registration End Date
+                    </h4>
                     <p>{EventPosterDetail5.RegistEndDate}</p>
                     <br />
                     <h4 className="fw-bolder">Location</h4>
@@ -92,7 +121,9 @@ function EventPoster5() {
                     <p>{EventPosterDetail5.EventCategory}</p>
                     <br />
                     <div className="event-share">
-                      <h4 className="fw-bolder">Share With Friends</h4>
+                      <h4 className="fw-bolder">
+                        Share With Friends
+                      </h4>
                       <a
                         href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
                           window.location.href
@@ -102,7 +133,7 @@ function EventPoster5() {
                       >
                         <FontAwesomeIcon
                           icon={faSquareFacebook}
-                          style={{ color: "#1b56bb" }}
+                          style={{ color: '#1b56bb' }}
                           className="event-share-logo"
                         />
                       </a>
@@ -115,7 +146,7 @@ function EventPoster5() {
                       >
                         <FontAwesomeIcon
                           icon={faSquareTwitter}
-                          style={{ color: "#0b8ee0" }}
+                          style={{ color: '#0b8ee0' }}
                           className="event-share-logo"
                         />
                       </a>
@@ -128,7 +159,7 @@ function EventPoster5() {
                       >
                         <FontAwesomeIcon
                           icon={faLinkedin}
-                          style={{ color: "#16b7df" }}
+                          style={{ color: '#16b7df' }}
                           className="event-share-logo"
                         />
                       </a>
@@ -141,7 +172,7 @@ function EventPoster5() {
                       >
                         <FontAwesomeIcon
                           icon={faSquarePinterest}
-                          style={{ color: "#d30909" }}
+                          style={{ color: '#d30909' }}
                           className="event-share-logo"
                         />
                       </a>
@@ -154,14 +185,16 @@ function EventPoster5() {
                     >
                       <FontAwesomeIcon
                         icon={showContent ? faMinus : faPlus}
-                        style={{ color: "#1164e8" }}
+                        style={{ color: '#1164e8' }}
                       />
                       <span className="ms-2">Venue</span>
                     </h1>
                     {showContent && (
                       <div className="row event-venue">
                         <div className="event-venue-left col-3">
-                          <img src={EventPosterDetail5.ImageVeneu}></img>
+                          <img
+                            src={EventPosterDetail5.ImageVeneu}
+                          ></img>
                         </div>
                         <div className="event-venue-right col-8 mt-auto mb-auto">
                           <a
@@ -171,7 +204,7 @@ function EventPoster5() {
                           >
                             <FontAwesomeIcon
                               icon={faLocationDot}
-                              style={{ color: "#1164e8" }}
+                              style={{ color: '#1164e8' }}
                             />
                             {EventPosterDetail5.NameLocation}
                           </a>
