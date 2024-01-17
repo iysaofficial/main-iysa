5fb574687d027a938c968b6d5804c743bc8c8cb7
young <youngscientist.iysa@gmail.com>
2024-01-17 13:59:43 +0700

    new

--- src/components/home/Vision-Mission.js
+++ src/components/home/Vision-Mission.js
@@ -5,8 +5,9 @@ const VisionMission = () => {
         <div className="card">
           <img src="./assets/images/icon/vision.png"></img>
           <div className="card-content">
-            <h3 className="text-center mb-3">Vision</h3>
-            <p>
+            <h3 className="text-center mb-1">Vision</h3>
+            <p className="garis-bawah mb-4"></p>
+            <p className="text-center">
               Become an institution that can facilitate Indonesian young
               researchers to continue communicate and innovate in conducting
               research.
@@ -16,14 +17,15 @@ const VisionMission = () => {
         <div className="card">
           <img src="./assets/images/icon/mission.png"></img>
           <div className="card-content">
-            <h3 className="text-center mb-3">Mission</h3>
+            <h3 className="text-center mb-1">Mission</h3>
+            <p className="garis-bawahi mb-4"></p>
             <ol>
-              <li>
+              <li className="text-center">
                 Implement a national level collaboration program between
                 Indonesian young researchers.
               </li>
-              <li className="mt-1">Conduct domestic competition programs.</li>
-              <li className="mt-1">
+              <li className="mt-1 text-center">Conduct domestic competition programs.</li>
+              <li className="mt-1 text-center">
                 Prepare Indonesian young scientists to represent Indonesian in
                 international level competitions.
               </li>
@@ -33,13 +35,13 @@ const VisionMission = () => {
         <div className="card">
           <img src="./assets/images/icon/goals.png"></img>
           <div className="card-content">
-            <h3 className="text-center mb-3">Goals</h3>
+            <h3 className="text-center mb-1">Goals</h3>
+            <p className="garis-bawah mb-4"></p>
             <ol>
-              <li>
-                Provide oppotunities for Indonesian students to develop their
-                potential, especially in the field of science & technology.
+              <li className="text-center">
+                Provide oppotunities for Indonesian students to develop their potential, especially in the field of science & technology.
               </li>
-              <li className="mt-1">
+              <li className="mt-1 text-center">
                 Cultivate the spirit of competition in order to optimize the
                 potential that exists in Indonesian students.
               </li>
